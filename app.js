795defc6a162470d0fc3c061391f1feabc179e97
NicholasHorta <nickdeveloper.js@gmil.com>
2022-04-04 20:21:12 +0100

    json configuration

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import path from 'path';
 const app = express();
 const appDebug = debug('app'); 
 const __dirname = path.resolve();
+const PORT = process.env.PORT 
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')))
@@ -16,14 +17,14 @@ app.use(express.static(path.join(__dirname, '/public/')))
 
 
 app.get('/', (req, res) => {
-    res.send('gay')
+    res.send('gay');
 })
 
 //. - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Paths
 
 
-app.listen(3500, () => {
-    appDebug(`${chalk.cyan('listening on port')} ${chalk.green('3500')}`)
+app.listen(PORT, () => {
+    appDebug(`${chalk.cyan('listening on port')} ${chalk.green(PORT)}`)
 })
 
 
@@ -43,6 +44,11 @@ app.listen(3500, () => {
 // We tell it where we will pull the static files from using the PATH package from express
 // Then we provide "where" on the file system where these static files are
 // __dirname - A variable from Node that results in "Where are you running from" for files < /Users/unicompare/Desktop/Node >
+
+/// PROCESS.ENV
+// process.env - What comes out of the environment 
+// So NODEMON is currently dropping in the value 3500 as PORT
+// process.env strips out whatever we dropped into the JSON object
 
 /// CHALK
 // Run debug for all || just app - DEBUG=* node app.js || DEBUG=app node app.js
